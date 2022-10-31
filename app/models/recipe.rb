4749c03e30bf7cb5617cd83388d5c1f565db369d
Hu Zheng <hz2709@columbia.edu>
2022-10-30 22:48:39 -0400

    Delete unsed.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ActiveRecord::Base
     # validates_uniqueness_of :name
+    belongs_to :cuisine
+    has_many :recipe_ingredients
+    has_many :ingredients, through: :recipe_ingredients
 end
