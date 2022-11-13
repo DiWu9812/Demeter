813ca16f358521820e886a22a608af148a8f9ef1
Jessica Wang <jessicawang@jessicas-mbp-7.myfiosgateway.com>
2022-11-13 11:26:31 -0500

    add draft signup login logout features

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221027152548) do
+ActiveRecord::Schema.define(version: 20221113145151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 20221027152548) do
   end
 
   add_index "recipes", ["origin_id"], name: "unique_recipe", unique: true, using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   add_foreign_key "recipe_ingredients", "ingredients"
   add_foreign_key "recipe_ingredients", "recipes"
